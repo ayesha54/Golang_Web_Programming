673ac731d2e980e6a14397cb68e3cb1444e08ef1
ayesha kaleem <akaleem306@gmail.com>
2020-05-06 07:15:04 +0500

    Go Templates

---
+++ Templates/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("template_files/*"))
+
+}
+func main() {
+	err := tpl.Execute(os.Stdout, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "index.html", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "home.html", nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
